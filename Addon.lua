2f7ac9ffaef50fe0a4c4217426abae32c026c326
PProvost <peter@provost.org>
2011-01-23 19:53:58 -0700

    Added a quick check to ensure the map exists

--- Addon.lua
+++ Addon.lua
@@ -20,12 +20,15 @@ local scale = 1.0
 local min, max = 0.5, 3.0
 
 local parent = BattlefieldMinimap
+if not parent then return end
+
 local f = CreateFrame("Frame", nil, parent)
 f:SetFrameStrata("LOW")
 f:EnableMouse(false)
 f:SetPoint("TOPLEFT", parent, "TOPLEFT")
 f:SetPoint("BOTTOMRIGHT", parent, "BOTTOMRIGHT")
 f:EnableMouseWheel(true)
+
 f:SetScript("OnMouseWheel", function(frame, delta)
 	if not IsAltKeyDown() then return end
 	if delta > 0 then 
@@ -37,5 +40,3 @@ f:SetScript("OnMouseWheel", function(frame, delta)
 	if scale < min then scale = min end
 	parent:SetScale(scale)
 end)
-
-
